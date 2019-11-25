7611ee89f40fb096ddef109437e16ed14a6eefd1
Dexter444 <qifengzhoufl@gmail.com>
2019-11-25 18:33:50 -0500

    stremaing data etl

--- FinalProjectCSYE7200/src/main/scala/Test.scala
+++ FinalProjectCSYE7200/src/main/scala/Test.scala
@@ -1,21 +1,27 @@
-import bean.Accident
+import bean.{Accident, Casualty}
 import data.DataSetCreation
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object Test {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("Test").setMaster("local[*]")
     val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+//
+//    val vehicleDS = DataSetCreation.getVehicleData("in/Vehicles.csv",spark)
+//    val accidentDS = DataSetCreation.getAccidentData("in/Accidents.csv",spark)
+//    val casualtyDS = DataSetCreation.getCasualtyData("in/Casualties.csv",spark)
+//
+//
+//    vehicleDS.show(5)
+//    accidentDS.show(5)
+//    casualtyDS.show(5)
+    val seq: Seq[Int] = Seq(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)
+    val value: RDD[Int] = spark.sparkContext.makeRDD(seq)
+    import spark.implicits._
 
-    val vehicleDS = DataSetCreation.getVehicleData("in/Vehicles.csv",spark)
-    val accidentDS = DataSetCreation.getAccidentData("in/Accidents.csv",spark)
-    val casualtyDS = DataSetCreation.getCasualtyData("in/Casualties.csv",spark)
-
-
-    vehicleDS.show(5)
-    accidentDS.show(5)
-    casualtyDS.show(5)
-
+    val ds: Dataset[Casualty] = seq.toDF().as[Casualty]
+    ds.show()
   }
 }
--- FinalProjectCSYE7200/src/main/scala/app/App.scala
+++ FinalProjectCSYE7200/src/main/scala/app/App.scala
@@ -1,8 +1,9 @@
 package app
 
+import bean.{Accident, Casualty, Vehicle}
 import data.DataSetCreation
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -14,22 +15,44 @@ object App {
       println("Usage: StatStreamingApp <zkQuorum> <group> <topics> <numThreads>")
       System.exit(1)
     }
-    val Array(zkQuorum, groupId, topics, numThreads) = args
+    // spark sql initialization
     val conf: SparkConf = new SparkConf().setAppName("CSYE7200FinalProject")
-    // Retrieve data once per minute
-    val streamingContext = new StreamingContext(conf,Seconds(60))
     val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+    import spark.implicits._
+    // Data from local system
+    val vehicleDS:Dataset[Vehicle] = DataSetCreation.getVehicleData("in/Vehicles.csv",spark)
+    val accidentDS:Dataset[Accident] = DataSetCreation.getAccidentData("in/Accidents.csv",spark)
+    val casualtyDS:Dataset[Casualty] = DataSetCreation.getCasualtyData("in/Casualties.csv",spark)
+
+    // streaming preparation
+    val Array(zkQuorum, groupId, topics, numThreads) = args
+    val streamingContext = new StreamingContext(conf,Seconds(30))
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
-    //get message from kafka
     val messages = KafkaUtils.createStream(streamingContext, zkQuorum, groupId, topicMap)
-    val logs = messages.map(_._2)
-    val vehicleDS = DataSetCreation.getVehicleData("in/Vehicles.csv",spark)
-    val accidentDS = DataSetCreation.getAccidentData("in/Accidents.csv",spark)
-    val casualtyDS = DataSetCreation.getCasualtyData("in/Casualties.csv",spark)
+    // etl
+    messages.map(_._2).map {
+      d => {
+        val data: Array[String] = d.split(",")
+        d.split(",").length match {
+          case 16 => {
+            val ds: Dataset[Casualty] = Seq(Casualty(data(0), data(1), data(2), data(3), data(4), data(5), data(6), data(7), data(8), data(9), data(10), data(11), data(12), data(13), data(14), data(15))).toDS()
+            casualtyDS.union(ds)
+          }
+          case 23 => {
+            val ds: Dataset[Vehicle] = Seq(Vehicle(data(0), data(1), data(2), data(3), data(4), data(5), data(6), data(7), data(8), data(9), data(10), data(11), data(12), data(13), data(14), data(15), data(16), data(17), data(18), data(19), data(20), data(21), data(22))).toDS()
+            vehicleDS.union(ds)
+          }
+          case 31 => {
+            val ds: Dataset[Accident] = Seq(Accident(data(0), data(1), data(2), data(3), data(4), data(5), data(6), data(7), data(8), data(9), data(10), data(11), data(12), data(13), data(14), data(15), data(16), data(17), data(18), data(19), data(20), data(21), data(22), data(23), data(24), data(25), data(26), data(27), data(28), data(29), data(30))).toDS()
+            accidentDS.union(ds)
+          }
+          case _ => println("Please check your data")
+        }
+      }
+    }
 
     // machine learning algo
     machineLearning
-
 
     streamingContext.start()
     streamingContext.awaitTermination()
